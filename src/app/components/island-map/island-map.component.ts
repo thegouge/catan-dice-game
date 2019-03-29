4ef0feebbac003fe19b62b3a867e730f48a2f04f
thegouge <thegouge42@gmail.com>
2019-03-28 22:01:17 -0700

    perfected building positioning

--- src/app/components/island-map/island-map.component.ts
+++ src/app/components/island-map/island-map.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Output, EventEmitter} from "@angular/core";
+import {Component, OnInit, Output, EventEmitter, Input} from "@angular/core";
 
 import {Building} from "../../models/Building";
 
@@ -8,47 +8,11 @@ import {Building} from "../../models/Building";
   styleUrls: ["./island-map.component.css", "./positioning.css"]
 })
 export class IslandMapComponent implements OnInit {
+  @Input() buildingList: Building[];
+
   @Output() scoreBuilding: EventEmitter<number> = new EventEmitter();
 
-  buildingList: Building[];
-
-  constructor() {
-    this.buildingList = [
-      new Building(0, "start", 0),
-      new Building(1, "settlement", 3),
-      new Building(2, "road", 1),
-      new Building(3, "road", 1),
-      new Building(4, "city", 7),
-      new Building(5, "road", 1),
-      new Building(6, "settlement", 4),
-      new Building(7, "road", 1),
-      new Building(8, "road", 1),
-      new Building(9, "city", 12),
-      new Building(10, "road", 1),
-      new Building(11, "settlement", 5),
-      new Building(12, "road", 1),
-      new Building(13, "road", 1),
-      new Building(14, "settlement", 7),
-      new Building(15, "road", 1),
-      new Building(16, "road", 1),
-      new Building(17, "settlement", 9),
-      new Building(18, "road", 1),
-      new Building(19, "road", 1),
-      new Building(20, "settlement", 11),
-      new Building(21, "road", 1),
-      new Building(22, "road", 1),
-      new Building(23, "city", 20),
-      new Building(24, "road", 1),
-      new Building(25, "road", 1),
-      new Building(26, "city", 30),
-      new Building(27, "soldier", 1),
-      new Building(28, "soldier", 2),
-      new Building(29, "soldier", 3),
-      new Building(30, "soldier", 4),
-      new Building(31, "soldier", 5),
-      new Building(32, "soldier", 6)
-    ];
-  }
+  constructor() {}
 
   ngOnInit() {}
 
