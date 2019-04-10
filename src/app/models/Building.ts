ca7fb664d09d338caa8e869c7a1333f50ccc6a3a
thegouge <thegouge42@gmail.com>
2019-04-09 20:22:57 -0700

    added costs to building class

--- src/app/models/Building.ts
+++ src/app/models/Building.ts
@@ -1,11 +1,40 @@
+import {Cost} from "./Cost";
+
 export class Building {
   built: boolean;
+  buildingCosts: any;
 
   constructor(
     public id: number,
     public type: string,
     public pointValue: number
   ) {
+    switch (type) {
+      case "road":
+        this.buildingCosts = {wood: 1, brick: 1};
+        break;
+
+      case "soldier":
+        this.buildingCosts = {wool: 1, wheat: 1, ore: 1};
+        break;
+
+      case "settlement":
+        this.buildingCosts = {wood: 1, brick: 1, wool: 1, wheat: 1};
+        break;
+
+      case "city":
+        this.buildingCosts = {wheat: 2, ore: 3};
+        break;
+
+      case "start":
+        this.built = true;
+        this.buildingCosts = {};
+        break;
+
+      default:
+        this.buildingCosts = {};
+        break;
+    }
     if (type === "start") {
       this.built = true;
     }
